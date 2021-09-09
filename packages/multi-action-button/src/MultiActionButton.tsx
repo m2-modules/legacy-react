ea66651099057c0a6df59d0f108130f9afcf8e90
Jay Lee <jaylee.possible@gmail.com>
2021-09-10 01:33:21 +0900

    Update multi action button

--- packages/multi-action-button/src/MultiActionButton.tsx
+++ packages/multi-action-button/src/MultiActionButton.tsx
@@ -1,17 +1,12 @@
+import MoreHorizontalIcon from '@material-ui/icons/MoreHoriz'
 import React from 'react'
-
-import styled from 'styled-components'
-
-import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
-
 import { SubButton } from '../interfaces'
+import styled from 'styled-components'
 
 const InlineFlexDiv = styled.div`
   display: inline-flex;
-  flex-direction: ${(props) => props.flexDirection || 'row'};
   border: 1px solid black;
   border-radius: 5px;
-  height: ${(props: { height?: string }) => props.height || '30px'};
 `
 
 const Button = styled.button`
@@ -19,14 +14,35 @@ const Button = styled.button`
   align-items: center;
   border: none;
   background-color: transparent;
+  height: ${(props: { height?: string }) => props.height || '30px'};
+  box-sizing: border-box;
 `
 
 const MoreButton = styled(Button)`
   border-inline-start: 1px solid black;
+  position: relative;
+  & > div.sub-button-container {
+    opacity: 0;
+  }
+  &:focus > div.sub-button-container {
+    opacity: 1;
+  }
 `
 
-const StyledMoreHorizIcon = styled(MoreHorizIcon)`
+const StyledMoreHorizontalIcon = styled(MoreHorizontalIcon)`
   font-size: ${(props) => props.fontSize || 'small'};
+`
+
+const SubButtonContainerDiv = styled(InlineFlexDiv)`
+  flex-direction: column;
+  position: absolute;
+  bottom: calc(100% + 10px);
+  right: 0px;
+  height: fit-content;
+  width: max-content;
+  &:focus {
+    opacity: 1 !important;
+  }
 `
 
 export declare type MultiActionButtonPropsType = {
@@ -46,16 +62,16 @@ const MultiActionButton = (props: MultiActionButtonPropsType): JSX.Element => {
       {subButtons?.length ? (
         <>
           <MoreButton onClick={openSubButtons}>
-            <StyledMoreHorizIcon fontSize="small" />
-          </MoreButton>
+            <StyledMoreHorizontalIcon fontSize="small" />
 
-          <InlineFlexDiv flexDirection="column">
-            {subButtons.map((subAction: SubButton, idx: number) => (
-              <Button key={`sub-button-${idx}`} onClick={subAction.action}>
-                {subAction.name}
-              </Button>
-            ))}
-          </InlineFlexDiv>
+            <SubButtonContainerDiv className="sub-button-container">
+              {subButtons.map((subAction: SubButton, idx: number) => (
+                <Button key={`sub-button-${idx}`} onClick={subAction.action}>
+                  {subAction.name}
+                </Button>
+              ))}
+            </SubButtonContainerDiv>
+          </MoreButton>
         </>
       ) : (
         ''

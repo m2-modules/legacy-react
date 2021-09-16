a49e95de53939e77cbd034c2e14ca878a819fff6
gincheong2 <gincheong2@gmail.com>
2021-09-16 09:57:08 +0900

    fix(floating-label-input): interfaces 변경, eslint warning 처리

--- packages/floating-label-input/stories/FloatingLabelInput.stories.tsx
+++ packages/floating-label-input/stories/FloatingLabelInput.stories.tsx
@@ -3,45 +3,44 @@ import React from 'react'
 import { ComponentMeta } from '@storybook/react'
 
 import FloatingLabelInput from '../src/FloatingLabelInput'
-import { FloatingLabelInputProps } from '../src/interfaces';
+import { FloatingLabelInputProps } from '../src/interfaces'
 
 export default {
   title: 'FloatingLabelInput',
   component: FloatingLabelInput,
   argTypes: {
     label: {
-      control: { type: 'text' }
+      control: { type: 'text' },
     },
     underlineColor: {
-      control: { type: 'color' }
+      control: { type: 'color' },
     },
     style: {
       width: {
-        control: { type: 'text' } 
-      }
+        control: { type: 'text' },
+      },
     },
-  }
+  },
 } as ComponentMeta<typeof FloatingLabelInput>
 
-export const Basic = (args: FloatingLabelInputProps) => {
-  const { label, underlineColor, ...rest } = args;
+export const Basic = (args: FloatingLabelInputProps): JSX.Element => {
+  const { label, underlineColor, ...rest } = args
 
-  const [value, setValue] = React.useState('');
+  const [value, setValue] = React.useState('')
 
   return (
     <FloatingLabelInput
       label={label}
       value={value}
       underlineColor={underlineColor}
-      onChange={event => setValue(event.target.value)}
+      onChange={(event) => setValue(event.target.value)}
       {...rest}
     />
-  );
-};
+  )
+}
 Basic.args = {
   style: {
     width: '300px',
   },
   label: 'This is Floating Label',
-};
-
+}

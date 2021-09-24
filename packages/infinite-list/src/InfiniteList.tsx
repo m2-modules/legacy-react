51df026400f4608f648b8eab6b0fac36828ed397
Jay Lee <jaylee.possible@gmail.com>
2021-09-25 04:07:44 +0900

    #58 close

--- packages/infinite-list/src/InfiniteList.tsx
+++ packages/infinite-list/src/InfiniteList.tsx
@@ -1,4 +1,10 @@
-import React, { UIEvent, useCallback, useEffect, useState } from 'react'
+import React, {
+  RefObject,
+  createRef,
+  useCallback,
+  useEffect,
+  useState,
+} from 'react'
 
 import styled from 'styled-components'
 
@@ -8,57 +14,62 @@ const StyledList = styled.ul`
 `
 
 export type InfiniteListPropsType = {
-  fetchHandler: <T>(page: number) => Promise<T[]> | T[]
+  fetchHandler: (page: number) => JSX.Element[] | Promise<JSX.Element[]>
   threshHoldRate?: number
   children?: JSX.Element[]
 }
 
 const InfiniteList = (props: InfiniteListPropsType): JSX.Element => {
   const { fetchHandler, threshHoldRate = 80 } = props
-  const [page, setPage] = useState<number>(1)
+  const listRef: RefObject<HTMLUListElement> = createRef<HTMLUListElement>()
+  const [page, setPage] = useState<number>(0)
   const [children, setChildren] = useState<JSX.Element[]>(props.children || [])
 
-  useEffect(() => {
-    async function initialFetch(): Promise<void> {
-      const newChildren: JSX.Element[] = await fetchHandler(1)
-      if (newChildren?.length) {
-        setChildren(newChildren)
-        setPage(2)
-      } else {
-        setChildren([])
-      }
+  const isFetchRequired = useCallback(() => {
+    const list: HTMLUListElement | null = listRef.current
+    if (!list) throw new Error('Failed to find list element')
+
+    if (list.hasAttribute('loading')) return false
+
+    if (list.scrollHeight <= list.clientHeight) return true
+
+    const currentScrollPosition: number = list.scrollTop + list.clientHeight
+    const scrollHeight: number = list.scrollHeight
+
+    return (currentScrollPosition / scrollHeight) * 100 >= threshHoldRate
+  }, [listRef, threshHoldRate])
+
+  const appendChild = useCallback(async (): Promise<void> => {
+    const list: HTMLUListElement | null = listRef.current
+    if (!list) throw new Error('Failed to find list element')
+
+    if (!isFetchRequired()) return
+
+    list.setAttribute('loading', '')
+    const newChildren: JSX.Element[] = await fetchHandler(page + 1)
+    if (newChildren?.length) {
+      setChildren([...children, ...newChildren])
+      setPage(page + 1)
     }
 
-    initialFetch()
-  }, [fetchHandler])
+    list.removeAttribute('loading')
+  }, [children, fetchHandler, isFetchRequired, listRef, page])
 
-  const onScrollHandler = useCallback(
-    async (event: UIEvent<HTMLOListElement | HTMLUListElement>) => {
-      if (typeof fetchHandler === 'function') {
-        const list: HTMLOListElement | HTMLUListElement = event.currentTarget
-
-        if (list.hasAttribute('loading')) return
-
-        const currentScrollPosition: number = list.scrollTop + list.clientHeight
-        const scrollHeight: number = list.scrollHeight
-
-        if ((currentScrollPosition / scrollHeight) * 100 >= threshHoldRate) {
-          list.setAttribute('loading', '')
-          const newChildren: JSX.Element[] = await fetchHandler(page + 1)
-          if (newChildren?.length) {
-            setChildren([...children, ...newChildren])
-            setPage(page + 1)
-          }
-
-          list.removeAttribute('loading')
-        }
+  useEffect(() => {
+    if (listRef.current) {
+      const list: HTMLUListElement = listRef.current
+      if (list.scrollHeight <= list.clientHeight) {
+        appendChild()
       }
-    },
-    [children, fetchHandler, page, threshHoldRate]
-  )
+    }
+  }, [appendChild, listRef])
+
+  const onScrollHandler = useCallback(async () => {
+    appendChild()
+  }, [appendChild])
 
   return (
-    <StyledList onScroll={onScrollHandler}>
+    <StyledList ref={listRef} onScroll={onScrollHandler}>
       {(children || []).map((child: JSX.Element, idx: number) => (
         <li key={`item-${idx}`}>{child}</li>
       ))}

b6bfe1831567046cb8b078f5ce524b1c4b22963b
Jay Lee <jaylee.possible@gmail.com>
2021-10-14 17:20:13 +0900

    Remove page props - #76

--- packages/infinite-list/stories/InfiniteList.stories.tsx
+++ packages/infinite-list/stories/InfiniteList.stories.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import styled from 'styled-components'
 
@@ -17,31 +17,33 @@ const SampleCard = styled.div`
   border: 1px solid #aaa;
 `
 
-const Template = (args: InfiniteListProps) => (
-  <InfiniteList {...args}></InfiniteList>
-)
+const Template = (args: InfiniteListProps) => {
+  const [cards, setCards] = useState<string[]>([])
+
+  return (
+    <InfiniteList
+      {...args}
+      onReach={async () => {
+        return new Promise((resolve) => {
+          console.log('Reached!')
+
+          setTimeout(() => {
+            if (cards.length >= 50) return resolve()
+
+            const newCards: string[] = Array(10)
+              .fill('')
+              .map((_: string, idx: number) => `Card ${cards.length + idx + 1}`)
+            setCards([...cards, ...newCards])
+            resolve()
+          }, 1000)
+        })
+      }}
+    >
+      {cards.map((text: string, idx: number) => (
+        <SampleCard key={idx}>{text}</SampleCard>
+      ))}
+    </InfiniteList>
+  )
+}
 
 export const Basic = Template.bind({})
-Basic.args = {
-  fetchHandler: async (page: number): Promise<JSX.Element[]> => {
-    return new Promise((resolve) => {
-      console.log('Fetch!')
-      console.log('Page: ' + page)
-      const limit = 20
-
-      setTimeout(() => {
-        page >= 30
-          ? resolve([])
-          : resolve(
-              Array(limit)
-                .fill('Card')
-                .map((text: string, idx: number) => (
-                  <SampleCard key={(page - 1) * limit + idx}>
-                    {text} {(page - 1) * limit + idx}
-                  </SampleCard>
-                ))
-            )
-      }, 1000)
-    })
-  },
-} as InfiniteListProps
